314ade7c13d4d6e6095de14529b4428c05b374d3
Aiden_yang <jayskateboy@outlook.com>
2020-05-09 17:26:26 +0800

    Initial commit

---
+++ Image_extraction.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+pic_4 = mpimg.imread('pic4.jpg')
+# pic_4.shape
+
+plt.imshow(pic_4)
+# plt.axis('off')
+plt.show()
