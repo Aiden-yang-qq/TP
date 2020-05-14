ef01d1fc2849632bad11d9238ab697b47da4c5c2
Aiden_yang <jayskateboy@outlook.com>
2020-05-14 11:21:03 +0800

    1.update

--- Image_extraction.py
+++ Image_extraction.py
@@ -1,22 +1,27 @@
+# 曲线提取 curve extraction
 import numpy as np
 import cv2
 
 
 def extraction(pic_):
     count = 0
+    # pic_b = 255
     x_list = []
     y_list = []
     pic = np.swapaxes(pic_, 1, 0)
     shape_i = pic.shape[0]
     shape_j = pic.shape[1]
-    for i in range(0, shape_i, 5):
-        for j in range(shape_j):
-            # if pic[i][j][0] <= 10 and pic[i][j][1] <= 10 and pic[i][j][2] >= 245:  # 选择想要的颜色区间
-            if 50 < pic[i][j][0] < 60 and 110 < pic[i][j][1] < 120 and 150 < pic[i][j][2] < 160:  # 选择想要的颜色区间
+    for i in range(0, shape_i, 2):
+        for j in range(1, shape_j):
+            # if pic[i][j][0] <= 100 and pic[i][j][1] <= 150 and pic[i][j][2] <= 220:  # 选择想要的颜色区间
+            # if pic[i][j][0] + pic[i][j][1] + pic[i][j][2] - pic[i][j - 1][0] - pic[i][j - 1][1] - pic[i][j - 1][2] > 500:
+            if int(pic[i][j-1][0]) - int(pic[i][j][0]) > 200:
                 count += 1
                 x_list.append(i)
                 # y_list.append(-1 * j)
                 y_list.append(j)
                 # print(i, j)
+                # pic_b = 255
                 break
+            # pic_b = pic[i][j][0]
     return x_list, y_list
--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import cv2
 from PIL import Image
 
 if __name__ == '__main__':
-    # pic_4 = mp_img.imread('pic4.jpg')
-    # xList, yList = Ie.extraction(pic_4)
-    I = Image.open('Figure_1.jpg')
-    # I = Image.open('pic4.jpg')
-    I.show()
-    L = I.convert('L')
-    L.show()
+    pic_4 = mp_img.imread('pic4.jpg')
+    xList, yList = Ie.extraction(pic_4)
 
-    # plt.figure()
-    # plt.plot(xList, yList, 'r.')
-    # plt.imshow(pic_4)
-    # plt.grid()
-    # plt.show()
+    # I = Image.open('Figure_1.jpg')
+    # # I = Image.open('pic4.jpg')
+    # I.show()
+    # L = I.convert('L')
+    # L.show()
+
+    plt.figure()
+    plt.plot(xList, yList, 'r.')
+    plt.imshow(pic_4)
+    plt.grid()
+    plt.show()
