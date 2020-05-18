6c4044842feed84bcfb993a60e0ef3cea0c443b8
Aiden_yang <jayskateboy@outlook.com>
2020-05-18 13:40:18 +0800

    1.新增灰度处理

--- Image_extraction.py
+++ Image_extraction.py
@@ -5,6 +5,11 @@ import numpy as np
 
 
 def extraction(pic_):
+    """
+    将图片矩阵中的曲线提取出来
+    :param pic_: 图片矩阵
+    :return: 曲线的横纵坐标
+    """
     count = 0
     x_list = []
     y_list = []
@@ -13,9 +18,9 @@ def extraction(pic_):
     shape_j = pic_new.shape[1]
     for i in range(0, shape_i):
         for j in range(5, shape_j, 5):
-            if int(pic_new[i][j - 5][0]) - int(pic_new[i][j][0]) > 195:
-                if int(pic_new[i][j - 5][1]) - int(pic_new[i][j][1]) > 135:
-                    if int(pic_new[i][j - 5][2]) - int(pic_new[i][j][2]) > 100:
+            if int(pic_new[i][j - 5][0]) - int(pic_new[i][j][0]) > 195:  # R
+                if int(pic_new[i][j - 5][1]) - int(pic_new[i][j][1]) > 135:  # G
+                    if int(pic_new[i][j - 5][2]) - int(pic_new[i][j][2]) > 95:  # B
                         count += 1
                         x_list.append(i)
                         y_list.append(j)
@@ -23,12 +28,35 @@ def extraction(pic_):
     return x_list, y_list
 
 
+def gray_scale(pic_):
+    """
+    将图片中的曲线灰度化
+    :param pic_: 图片矩阵
+    :return: 灰度化后的图片矩阵
+    """
+    pic__ = np.array(pic_)
+    pic__.flags.writeable = True    # 将矩阵读写置为可读
+    shape_i = pic__.shape[0]
+    shape_j = pic__.shape[1]
+    for i in range(shape_i):
+        for j in range(shape_j):
+            if pic__[i][j][0] <= 60 and pic__[i][j][1] <= 120 and pic__[i][j][2] <= 160:
+                pic__[i][j] = np.array([0, 0, 0], dtype='uint8')
+            else:
+                pic__[i][j] = np.array([255, 255, 255], dtype='uint8')
+    return pic__
+
+
 if __name__ == '__main__':
     # pic = mp_img.imread('pic4.jpg')
     pic = mp_img.imread('Figure_1.jpg')
-    # plt.imshow(pic)
-    # plt.show()
+    pic_n = gray_scale(pic)
+    plt.imshow(pic_n)
 
-    xList, yList = extraction(pic)
-    x_arr = np.array(xList)
-    y_arr = np.array(yList)
+    # xList, yList = extraction(pic)
+    # x_arr = np.array(xList)
+    # y_arr = np.array(yList)
+    #
+    # plt.imshow(pic)
+    # plt.plot(x_arr, y_arr, 'ro')
+    # plt.show()
