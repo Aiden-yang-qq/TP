18e9f041c62e131718c942ebd82fc2b7d9e1350a
Aiden_yang <jayskateboy@outlook.com>
2020-05-18 17:07:24 +0800

    1.加噪更新

--- add_noise.py
+++ add_noise.py
@@ -5,17 +5,20 @@ import sys
 
 
 def noise(y_data, n):
-    y_data_n = [y_data[0]]
+    # y_data_n = [y_data[0]]
+    y_data_n = []
+    y_data_len = len(y_data)
+    count = int(y_data_len / (n + 1))
     try:
-        for i in range(1, len(y_data) - 1):
-            if i % int((len(y_data) - 1) / n) == 0:
+        for i in range(y_data_len):
+            if i % count == 0:
                 rad = random.randint(20, 50)
                 if random.random() <= 0.5:
                     rad *= -1
                 y_data_n.append(y_data[i] + rad)
             else:
                 y_data_n.append(y_data[i])
-        y_data_n.append(y_data[-1])
+        # y_data_n.append(y_data[-1])
         return y_data_n
     except:
         print('Unexpected error:', sys.exc_info()[0])
@@ -25,13 +28,22 @@ def noise(y_data, n):
 if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
+    import matplotlib.image as mp_img
+    from Image_extraction import gray_scale, extraction
 
-    x = np.arange(-2 * np.pi, 2 * np.pi, 0.1 * np.pi)
-    y = np.sin(x)
-    y_n = noise(y, 9)
+    # x = np.arange(-2 * np.pi, 2 * np.pi, 0.1 * np.pi)
+    # y = np.sin(x)
+    pic = mp_img.imread('Figure_1.jpg')
+    # pic_n = gray_scale(pic)
+    plt.imshow(pic)
 
-    plt.figure()
-    plt.plot(x, y, 'ro')
+    xList, yList = extraction(pic)
+    x_arr = np.array(xList)
+    y_arr = np.array(yList)
+
+    y_n = noise(y_arr, 5)
+
+    # plt.figure()
+    plt.plot(x_arr, y_arr, 'ro')
     plt.grid()
-    # y_n = noise(y, 3)
-    plt.plot(x, y_n)
+    plt.plot(x_arr, y_n)
