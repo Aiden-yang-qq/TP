17e81018c881e738a158770fee82a79db98421bd
Aiden_yang <jayskateboy@outlook.com>
2020-06-11 10:19:58 +0800

    1.配置文件修改

--- Agreement/config/__init__.py
+++ Agreement/config/__init__.py
@@ -1,25 +1,33 @@
 from configparser import ConfigParser
 
 
-def config():
-    cp = ConfigParser()
-    # 以.cfg结尾的配置文件
-    # cp.read('config.cfg')
-    # 以.ini结尾的配置文件
-    cp.read('config.ini')
+class ConfigInfo:
+    def __init__(self):
+        # self.cp = cp
+        # self.file_name = file_name
+        self.cp = ConfigParser()
+        self.cp.read('config.ini')
 
-    # 获取mysql中的host值
-    host = cp.get('mysql', 'host')
-    print(host)
+    def first_scan(self):
+        first_scan = self.cp.get('SCAN', 'first_scan')  # SCAN中的FIRST_SCAN值
+        print('1:', first_scan, type(first_scan))
+        fs = str_to_bool(first_scan)
+        print('fs:', fs, type(fs))
+        if str_to_bool(first_scan):
+            print('2:', fs, type(fs))
+            self.cp.set('SCAN', 'first_scan', 'False')  # 设置SCAN中FIRST_SCAN的值
+            self.cp.write(open('config.ini', 'w'))  # 将新的值写入.ini配置文件中
+            re_get = self.cp.get('SCAN', 'first_scan')
+            print('3:', re_get, type(re_get))
+        else:
+            print('else')
+        print(self.cp.get('SCAN', 'first_scan'))
 
-    # 获取mysql中的db值
-    db = cp.get('mysql', 'db')
-    print(db)
 
-
-def main():
-    config()
+def str_to_bool(string):
+    return True if string.lower() == 'true' else False
 
 
 if __name__ == '__main__':
-    main()
+    conf = ConfigInfo()
+    conf.first_scan()
