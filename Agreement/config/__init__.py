764cb93dd73c907cafd76550d52f4e8b8ae32d14
Aiden_yang <jayskateboy@outlook.com>
2020-06-11 10:25:23 +0800

    1.配置文件修改

--- Agreement/config/__init__.py
+++ Agreement/config/__init__.py
@@ -3,25 +3,17 @@ from configparser import ConfigParser
 
 class ConfigInfo:
     def __init__(self):
-        # self.cp = cp
-        # self.file_name = file_name
         self.cp = ConfigParser()
         self.cp.read('config.ini')
 
     def first_scan(self):
         first_scan = self.cp.get('SCAN', 'first_scan')  # SCAN中的FIRST_SCAN值
-        print('1:', first_scan, type(first_scan))
         fs = str_to_bool(first_scan)
-        print('fs:', fs, type(fs))
-        if str_to_bool(first_scan):
-            print('2:', fs, type(fs))
+        if fs:
             self.cp.set('SCAN', 'first_scan', 'False')  # 设置SCAN中FIRST_SCAN的值
             self.cp.write(open('config.ini', 'w'))  # 将新的值写入.ini配置文件中
-            re_get = self.cp.get('SCAN', 'first_scan')
-            print('3:', re_get, type(re_get))
         else:
             print('else')
-        print(self.cp.get('SCAN', 'first_scan'))
 
 
 def str_to_bool(string):
